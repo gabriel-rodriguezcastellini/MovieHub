6b94fc1425547bd31fe33f4f709cdc7940207e0e
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-12-04 01:28:02 -0300

    refactor: Move database connection to src/index.ts and remove api/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { json, Request, Response } from "express";
 import cors from "cors";
 import router from "./routes/index";
-// import connectDB from "./database";
+import connectDB from "./database";
 import dotenv from "dotenv";
 
 console.log("running");
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-// connectDB();
+connectDB();
 
 app.use("/", (req, res) => {
   res.send("Hello World");
