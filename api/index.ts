2a2734aa6d5f78dadd22c002741c7bd5fb03dff8
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-12-04 01:09:41 -0300

    feat: Implement Express server with Hello World route and CORS support

---
+++ api/index.ts
@@ -0,0 +1,30 @@
+import express, { json, Request, Response } from "express";
+import cors from "cors";
+import router from "../src/routes/index";
+// import connectDB from "./database";
+import dotenv from "dotenv";
+
+console.log("running");
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+// connectDB();
+
+app.use("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).send("Route not found");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
